8b36cbf482b22b5e60447a3988dbb7a9dc79bf1a
Matheus Correia <matheus.correia@finderup.com.br>
2024-06-11 09:18:26 -0300

    wip: aditional filter modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,15 @@ export default async function Home({
   const movieGenres = await getMovieGenres();
 
   return (
-    <main className="bg-zinc-900 w-full h-full items-center flex justify-center flex-col">
-      <div className="w-full md:max-w-md flex gap-4 flex-col p-4">
+    <main className="bg-zinc-900 w-full h-full items-center flex justify-center flex-col py-12 px-4">
+      <div className="w-full md:max-w-md flex gap-4 flex-col">
         <label className="text-zinc-300 font-bold text-sm">
           Select the movie genres you would like to watch...
         </label>
         <MovieGenres genres={movieGenres.genres} />
+        <label className="text-xs underline text-zinc-300 hover:text-zinc-500 transition-all cursor-pointer">
+          Aditional filters
+        </label>
         <RandomMovie genreIds={searchParams.genreIds as string} />
         <GenerateRandomMovie />
       </div>
